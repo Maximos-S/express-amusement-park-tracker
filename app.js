9718a5f603bfd974a30bf247d3538890d8cfba2e
dale.sakamoto <daletsakamoto@gmail.com>
2020-10-23 13:40:25 -0700

    Phase-2 Done

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+const { router } = require('./routes')
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(morgan('dev'));
+app.use(router);
+
+app.use((req, res, next) => {
+  const err = new Error('The requests page couldnn\'t be found');
+  err.status = 404;
+  next(err);
+})
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404)
+    res.render('page-not-found', {title: 'Page Not Found'})
+  }
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.render('error', { title: 'Server Error', err })
+})
+
+
+module.exports = app
--- routes.js
+++ routes.js
@@ -5,3 +5,12 @@ const router = express.Router();
 router.get("/", (req, res) => {
     res.render("index", { title: "Home" })
 })
+
+    if (process.env.NODE_ENV !== 'production') {
+        router.get('/error-test', () => {
+          throw new Error('This is a test error')
+        })
+      } 
+
+
+module.exports = {router}
